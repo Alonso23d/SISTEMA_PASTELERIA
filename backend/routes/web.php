7d1179c97f17593aa8f375b283c9782d1dab875b
Jeffers1227 <silvazavalaj2@gmail.com>
2026-07-12 16:09:36 -0500

    Agrega ruta para migrar la BD en produccion

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/migrar-bd-secreta', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    return "¡Las tablas se han creado exitosamente en Clever Cloud!";
 });
