418c3e8ac50b971a7e178286fd0e5cc2da71b267
Jeffers1227 <silvazavalaj2@gmail.com>
2026-07-12 17:19:49 -0500

    Agrega ruta secreta para ejecutar los seeders y llenar la BD

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -11,3 +11,7 @@ Route::get('/migrar-bd-secreta', function () {
     Artisan::call('migrate', ['--force' => true]);
     return "¡Las tablas se han creado exitosamente en Clever Cloud!";
 });
+Route::get('/llenar-bd-secreta', function () {
+    Artisan::call('db:seed', ['--force' => true]);
+    return "¡Los pasteles y datos de prueba se han inyectado exitosamente en Clever Cloud!";
+});
